0582e3ff45809eb09a9c490c563b747bbeaba5b4
Moaz Ahmed <moazahmead34@gmail.com>
2025-12-15 20:06:39 +0200

    Add files via upload

---
+++ Bit++/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int x = 0;
+    while (n--) {
+        string operation;
+        cin >> operation;
+
+        if (operation == "++X" || operation == "X++")
+            x++;
+        else
+            x--;
+    }
+
+    cout << x << endl;
+    return 0;
+}
